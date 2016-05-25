d57546d2e1fa65eebb1046d1839755694a4b0bc9
Partsaleva <m_partsaleva@abv.bg>
2016-05-25 10:01:12 +0300

    Added gui

---
+++ 365days/src/main/Menu.java
@@ -0,0 +1,14 @@
+package main;
+
+import java.io.IOException;
+
+import fileControl.FileAccess;
+
+public class Menu {
+
+	public void startNewYear() throws IOException{
+		FileAccess files=new FileAccess();
+		files.createFileForYear("files/work","files/work365");
+		files.createFileForYear("files/holiday","files/holiday365");
+	}
+}
