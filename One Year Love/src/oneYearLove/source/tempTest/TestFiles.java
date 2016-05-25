29fd37809cb779c3e5127cea7ce83ccd417c4ca8
Partsaleva <m_partsaleva@abv.bg>
2016-05-25 10:22:03 +0300

    Org

---
+++ One Year Love/src/oneYearLove/source/tempTest/TestFiles.java
@@ -0,0 +1,45 @@
+package oneYearLove.source.tempTest;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class TestFiles {
+
+	public static void main(String[] args) {
+		TestFiles test=new TestFiles();
+		test.workFile();
+		test.holidayFile();
+	}
+	
+	public void createWorkdayFile(){
+		
+	}
+	
+	public void createHolidayFile(){
+		
+	}
+	
+	private void workFile(){
+		try(FileWriter f=new FileWriter("work.txt")){
+			for (int i = 1; i < 101; i++) {
+				f.write("WorkdayTicket"+ "_"+i);
+				f.write(System.getProperty("line.separator"));
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}		
+		
+	}
+	
+	private void holidayFile(){
+		try(FileWriter f=new FileWriter("holiday.txt")){
+			for (int i = 1; i < 101; i++) {
+				f.write("HolidayTicket"+ "_"+i);
+				f.write(System.getProperty("line.separator"));
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}		
+		
+	}
+}
